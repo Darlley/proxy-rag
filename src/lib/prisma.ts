b8a9bd138597bad19f2c79f7043e3e4db410021f
Darlley <systembrtt@gmail.com>
2024-10-13 22:20:54 -0400

    fix: prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+const connectionString = `${process.env.DATABASE_URL}`
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export default prisma
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
