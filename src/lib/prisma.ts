9ae96e18469f589b0be49aa31f660de40625ce85
Darlley <systembrtt@gmail.com>
2024-10-14 14:10:15 -0400

    fix: prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,6 +9,18 @@ dotenv.config();
 neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-const prisma = new PrismaClient({ adapter });
+const prismaClientSingleton = () => {
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaNeon(pool);
+  return new PrismaClient({ adapter });
+};
+
+declare global {
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+}
+
+const prisma = global.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') global.prismaGlobal = prisma;
