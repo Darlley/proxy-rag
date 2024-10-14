32c821d69f3dfbbe8f5757e0e065bb7b8bedb2e6
Darlley <systembrtt@gmail.com>
2024-10-13 21:46:03 -0400

    fix: prisma

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -5,7 +5,7 @@ export const ragChat = new RAGChat({
   model: upstash('meta-llama/Meta-Llama-3-8B-Instruct'),
   redis: redis,
   promptFn: ({ context, question, chatHistory }) =>
-    `Você é um assistente que responde perguntas sobre os artigos do site TabNews.
+    `Você é um assistente que responde perguntas sobre os artigos da url do usuário.
     Responda em português (pt-BR) e seja breve na resposta. 
     Não fuja do contexto. Pode usar emojis.
       ------
